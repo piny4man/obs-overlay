ace4434839b04c4b19609712abe06aa7ddea40a2
piny4man <development@pinya.dev>
2023-10-05 23:29:50 +0200

    feat: add htmx and test simple interactions

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
+use std::path::PathBuf;
+
 use askama::Template;
-use axum::{routing::get, Router};
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
+use tower_http::services::ServeDir;
 
 #[derive(Template)]
 #[template(path = "hello.html")]
-
 struct HelloTemplate<'a> {
     name: &'a str,
 }
 
-async fn hello_world() -> HelloTemplate<'static> {
-    HelloTemplate { name: "world" }
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
+}
+
+async fn hello_world() -> impl IntoResponse {
+    let template = HelloTemplate { name: "world" };
+    HtmlTemplate(template)
+}
+
+async fn hello_from_the_server() -> &'static str {
+    "Hello!"
 }
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let api_router = Router::new().route("/hello", get(hello_from_the_server));
+    let router = Router::new()
+        .nest("/api", api_router)
+        .route("/", get(hello_world))
+        .nest_service("/assets", ServeDir::new(PathBuf::from("assets")));
 
     Ok(router.into())
 }
